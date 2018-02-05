10a3304df3e3633a7db253996d0d544f7b0a5723
MikiSchikora <miki.s.t@hotmail.com>
2018-02-05 12:44:22 +0100

    New bla bla

--- Common_web_files/search_RecName.php
+++ Common_web_files/search_RecName.php
@@ -1,82 +1,60 @@
 <?php
+
+// THIS CODE IS FOR CHECKING RECCOMENDED NAME
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
 //load global vars and includes
-include "globals.inc.php";
+include "select_species.inc.php";
 
 //Store input data in $_SESSION to reaload initial form if necessary
-
-// My query is this:
-//print $_REQUEST['myQuery'];
-
-$_SESSION['queryData'] =$_REQUEST;
-
+$_SESSION['queryData'] = $_REQUEST;
 //If I want to redirect the query to another script
 //if ($_REQUEST['myQuery']) {
 //    header('Location: search.php?myQuery=' . $_REQUEST['myQuery']);
 //}
 
 
+
+// THE CODE BELOW GENERATES THE RECCOMENDED NAME OUTPUT
 //First make sure that the option RecName has been selected in the form
-if(isset($_REQUEST['RecName'])){
+if(isset($_REQUEST['RecName'])){   
     
+    // Loop through species
+    foreach ($Species as $Specie){
+        
+        $geneRecName = Null;
+        $protRecName = Null;
+
+        // Gene RecName
+        $sql = "SELECT g.gene_recommended_name FROM Gene g, Species sp, GeneSynonyms gsyn, ProteinSynonyms psyn, Proteins p "
+            . "WHERE g.tax_id = sp.tax_id AND g.id_ENTREZGENE = gsyn.id_ENTREZGENE AND g.id_ENTREZGENE = p.id_ENTREZGENE AND p.id_Uniprot = psyn.id_Uniprot "
+            . "AND sp.common_name like '%".$Specie."%' AND (g.gene_recommended_name = '".$query."' OR gsyn.name_genesynonym = '".$query."' OR g.id_ENTREZGENE = '".$query."' OR psyn.name_proteinsynonym = '".$query."' OR p.prot_recommended_name = '".$query."' OR p.id_Uniprot = '".$query."');";       
+        $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
+        if($rs){$geneRecName = mysqli_fetch_assoc($rs)['gene_recommended_name'];}
+
+        // Protein RecName
+        $sql = "SELECT p.prot_recommended_name FROM Gene g, Species sp, GeneSynonyms gsyn, ProteinSynonyms psyn, Proteins p "
+            . "WHERE g.tax_id = sp.tax_id AND g.id_ENTREZGENE = gsyn.id_ENTREZGENE AND g.id_ENTREZGENE = p.id_ENTREZGENE AND p.id_Uniprot = psyn.id_Uniprot "
+            . "AND sp.common_name like '%".$Specie."%' AND (g.gene_recommended_name = '".$query."' OR gsyn.name_genesynonym = '".$query."' OR g.id_ENTREZGENE = '".$query."' OR psyn.name_proteinsynonym = '".$query."' OR p.prot_recommended_name = '".$query."' OR p.id_Uniprot = '".$query."');";       
+        $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
+        if($rs){$protRecName = mysqli_fetch_assoc($rs)['prot_recommended_name'];}
+
+        
+        
+        
+        if ($protRecName or $geneRecName){
+            $something_printed = 1;
+            print(" <h3> $Specie </h3> ");      
+            if ($geneRecName){ print(" <h5> Gene Recommended Name: $geneRecName <br></h5> ");}
+            if ($protRecName){ print(" <h5> Protein Recommended Name: $protRecName <br></h5> ");}
+        }
+    }    
     
-//Start by looking at the Gene recommended name
-//Accepted inputs: ENTREZGENE, recommended name or synonym
-$sql_gene= "select g.gene_recommended_name from Gene g, GeneSynonyms gsyn where "
-        . "g.id_ENTREZGENE = gsyn.id_ENTREZGENE AND "
-        . "(gsyn.name_genesynonym = '".$_REQUEST['myQuery']."' OR "
-        . "g.gene_recommended_name = '".$_REQUEST['myQuery']."' or "
-        . "g.id_ENTREZGENE = '".$_REQUEST['myQuery']."');";
-
-
-//$sql = "SELECT g.gene_recommended_name from Gene g where g.id_ENTREZGENE='".$_REQUEST['myQuery']."'";
-
-
-//Query the database
-$rs = mysqli_query($mysqli, $sql_gene) or print mysqli_error($mysqli);
-
-
-//Print the results if found (How to do something after a checkbox is marked)
-if (mysqli_num_rows($rs)) {
-    $data = mysqli_fetch_assoc($rs);
-    print headerDBW($_REQUEST['myQuery']);   
-    ?>
-    <h4>You searched: <?php print $_REQUEST['myQuery']?></h4>
-    <h4>The recommended name is <?php print $data["gene_recommended_name"]?></h4>
-            
-    <?php
-    print footerDBW();
-
-} else {
-    //no gene found
-    print "no gene found";
-    $sql_protein = "select p.prot_recommended_name from Proteins p, ProteinSynonyms psyn, Gene g where "
-        . "g.id_ENTREZGENE = p.id_ENTREZGENE AND p.id_Uniprot = psyn.id_Uniprot AND "
-        . "(psyn.name_proteinsynonym = '".$_REQUEST['myQuery']."' OR "
-        . "p.prot_recommended_name = '".$_REQUEST['myQuery']."' or "
-        . "p.id_Uniprot = '".$_REQUEST['myQuery']."');";
-
-    $rs_prot = mysqli_query($mysqli, $sql_protein) or print mysqli_error($mysqli);
-    
-    //no gene nor protein found
-    if (!mysqli_num_rows($rs_prot)){
-        print errorPage('Not found','The requested name is not found');
-    } else {
-        //protein found
-        $data = mysqli_fetch_assoc($rs_prot);    
-        print headerDBW($_REQUEST['myQuery']);
-        ?>
-        <h4>You searched: <?php print $_REQUEST['myQuery']?></h4>
-        <h4>The recommended name is <?php print $data["prot_recommended_name"]?></h4>
-        <?php   
-        print footerDBW();
-   
-    } //protein rec name found
-    
-} //no gene found
-
-
-} else {print "Recommended name not selected";}
-
-?>
+    // You may not find anything in the selected specie, but it is found in another specie
+    if(!isset($something_printed)){
+        print "<h4> <br> Your search gave no results </h4>";       
+    }
+}
+else {print "<h4> <br> Recommended name not selected </h4>";}
