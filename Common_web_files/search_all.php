90d7590a01fb3a56170ed5b2540d1ab74bf1adb4
marinallealcustey <35842087+marinallealcustey@users.noreply.github.com>
2018-02-14 13:43:34 +0100

    print by search categories, all species inside it

--- Common_web_files/search_all.php
+++ Common_web_files/search_all.php
@@ -1,7 +1,5 @@
 <?php
-
-// THIS CODE IS FOR CHECKING RECCOMENDED NAME
-
+// THIS CODE IS FOR SEARCHING ALL
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -15,11 +13,15 @@ $_SESSION['queryData'] = $_REQUEST;
 //    header('Location: search.php?myQuery=' . $_REQUEST['myQuery']);
 //}
 
+$array;
+$info;
+$items;
+
 // Loop through species
 foreach ($Species as $Specie){
-
-    $geneRecName = Null;
-    $protRecName = Null;
+$info=array();
+//    $geneRecName = Null;
+//    $protRecName = Null;
 
     // Get information about your query in your specie:
     
@@ -29,18 +31,52 @@ foreach ($Species as $Specie){
         . "AND sp.common_name like '%".$Specie."%' AND (g.gene_recommended_name = '".$query."' OR gsyn.name_genesynonym = '".$query."' OR g.id_ENTREZGENE = '".$query."' OR psyn.name_proteinsynonym = '".$query."' OR p.prot_recommended_name = '".$query."' OR p.id_Uniprot = '".$query."');";       
     
     $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli); 
-         
+    
+
+    
+    
     $GeneSynonyms = array();
     $ProteinSynonyms = array();
+    $geneRecName=array();
+    $protRecName=array();
     
+    $GeneID= array();      
+    $ProteinID=array();
+
+
+if (!mysqli_num_rows($rs)) { 
+    $info["Gene recommended name"]=$geneRecName;
+    $info["Protein recommended name"]=$protRecName;
+    $info["Gene synonyms"]=$GeneSynonyms;
+    $info["Protein synonyms"]=$ProteinSynonyms;
+    //// AQUÍ FALTEN LA RESTA D'ITEMS !!!!!! ORTOLEGS ET AL
+    $array[$Specie]= $info;
+
+    continue;
+}
+    
+    
+
+
+    $i=0;
     while ($rsF = mysqli_fetch_array($rs)) {
-        $geneRecName = $rsF['gene_recommended_name'];
-        $protRecName = $rsF['prot_recommended_name'];
+        while ($i===0){
+            $geneRecName[] = $rsF['gene_recommended_name'];
+            $protRecName[] = $rsF['prot_recommended_name'];
+            $GeneID[] = $rsF['id_ENTREZGENE'];      
+            $ProteinID[] = $rsF['id_Uniprot']; 
+            $i++;
+        }
+        
         $GeneSynonyms[] = $rsF['name_genesynonym'];
         $ProteinSynonyms[] = $rsF['name_proteinsynonym'];
-        $GeneID = $rsF['id_ENTREZGENE'];      
-        $ProteinID = $rsF['id_Uniprot'];               
+        
     } 
+ 
+    
+    $GeneSynonyms=  array_unique($GeneSynonyms);
+    $ProteinSynonyms=  array_unique($ProteinSynonyms);
+
     
     //RECCOMENDED NAMES:
     
@@ -48,20 +84,66 @@ foreach ($Species as $Specie){
         
         if ($protRecName or $geneRecName){
             $something_printed = 1;
-            print(" <h3> $Specie </h3> ");      
-            if ($geneRecName){ print(" <h5> Gene Recommended Name: $geneRecName <br></h5> ");}
-            if ($protRecName){ print(" <h5> Protein Recommended Name: $protRecName <br></h5> ");}
+            //$info['Specie']=$Specie;   
+            if ($geneRecName){
+                $info["Gene recommended name"]=$geneRecName;
+                $items[]="Gene recommended name";
+            }
+            if ($protRecName){
+                $info["Protein recommended name"]=$protRecName;
+                $items[]="Protein recommended name";            
+            }
         }
     
     }
     
     if(isset($_REQUEST['Synonyms'])){
         
-        print "here go synonyms";
+        //print "here go synonyms<br>";
+        if ($GeneSynonyms or $ProteinSynonyms){
+            $something_printed = 1;
+            if ($GeneSynonyms){
+                $info["Gene synonyms"]=$GeneSynonyms;
+                $items[]="Gene synonyms";
+            }
+            if ($ProteinSynonyms){
+                $info["Protein synonyms"]=$ProteinSynonyms;
+                $items[]="Protein synonyms";
+            }
+        }
         
     }  
+    
+    //$Specie;
+    
+    $array[$Specie]= $info;
+
+    
+
+      
+      
 } 
 
+
+$items=  array_unique($items);
+
+    foreach ($items as $t) {
+        print "<br><h3>".$t."</h3><br>";
+        foreach ($Species as $s){
+            print "<h4>".$s."</h4><br>";
+            foreach ($array[$s][$t] as $final){
+                print_r ($final);
+                print "<br>";
+            }
+                        
+        }
+                
+                
+    }
+
+print "<br>";
+print footerDBW();   
+    
 // Debug if you didn't find anything
 if (!$something_printed){
     print(" <h5> Your search gave no results </5>");
