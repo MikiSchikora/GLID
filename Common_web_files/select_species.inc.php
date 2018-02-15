a2496be13abbec4dbf58fb357103c26662bdac6f
MikiSchikora <miki.s.t@hotmail.com>
2018-02-15 16:39:57 +0100

    New select_species

--- Common_web_files/select_species.inc.php
+++ Common_web_files/select_species.inc.php
@@ -58,39 +58,13 @@ else{
 
 }
 
+// If a specie has ', correct it:
+$Final_species = array();
+foreach ($Species as $s){
+    $Final_species[] = str_replace("'","\'",$s);
+}
+$Species = $Final_species;
+
 
 // Define the type of query (may be more than one):    
 $query = $_REQUEST['myQuery'];
-
-/*
-$query_gene  = 0; $query_prot  = 0 ;$query_gsyn  = 0 ;$query_psyn  = 0;
-
-// search database for gene_recommended_name
-$sql = "SELECT g.gene_recommended_name from Gene g WHERE g.gene_recommended_name = '".$query."'";
-$rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
-if (mysqli_num_rows($rs)){ $query_gene = 1;}
-
-// search database for gene_synonyms
-$sql = "SELECT gsyn.name_genesynonym from GeneSynonyms gsyn WHERE gsyn.name_genesynonym = '".$query."'";
-$rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
-if (mysqli_num_rows($rs)){ $query_gsyn = 1;}  
-
-if ($query_gene==0 and $query_gsyn==0){
-    
-    // search database for protein_recommended_name
-    $sql = "SELECT p.prot_recommended_name from Proteins p WHERE p.prot_recommended_name = '".$query."'";
-    $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);
-    if (mysqli_num_rows($rs)){ $query_prot = 1;}
-
-    // search database for prot_synonyms
-    $sql = "SELECT psyn.name_proteinsynonym from ProteinSynonyms psyn WHERE psyn.name_proteinsynonym = '".$query."'";
-    $rs = mysqli_query($mysqli, $sql) or print mysqli_error($mysqli);  
-    if (mysqli_num_rows($rs)){ $query_psyn = 1;}
-}
-// nothing found
-if ($query_gene == 0 and $query_prot == 0 and $query_gsyn == 0 and$query_psyn == 0 ){
-    print(" <h4> Your search gave no results, try again... </h4> ");
-    exit();    
-}
-
- */
